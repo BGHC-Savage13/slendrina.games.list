304ed65e6d7403c7d89f36d026241a2fa96d975e
Relyx <149301530+Hacsu8166Alt@users.noreply.github.com>
2023-11-18 10:29:26 +0500

    Create the.list.lua

---
+++ the.list.lua
@@ -0,0 +1,90 @@
+--fullscreen functions
+function fullscreen then get "https://github.com/Hacsu8166Alt/slendrina.games.list/new/main" then do fullscreen function then print 'FULLSCREEN SUCCES'   then do list function write text then break do then fullscreen()
+
+
+
+
+██╗░░░░░██╗░██████╗████████╗  ░█████╗░███████╗  ░██████╗░░█████╗░███╗░░░███╗███████╗░██████╗
+██║░░░░░██║██╔════╝╚══██╔══╝  ██╔══██╗██╔════╝  ██╔════╝░██╔══██╗████╗░████║██╔════╝██╔════╝
+██║░░░░░██║╚█████╗░░░░██║░░░  ██║░░██║█████╗░░  ██║░░██╗░███████║██╔████╔██║█████╗░░╚█████╗░
+██║░░░░░██║░╚═══██╗░░░██║░░░  ██║░░██║██╔══╝░░  ██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░░╚═══██╗
+███████╗██║██████╔╝░░░██║░░░  ╚█████╔╝██║░░░░░  ╚██████╔╝██║░░██║██║░╚═╝░██║███████╗██████╔╝
+╚══════╝╚═╝╚═════╝░░░░╚═╝░░░  ░╚════╝░╚═╝░░░░░  ░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝╚═════╝░
+
+░░██╗██████╗░██╗░░░██╗██╗░░░░░░█████╗░██████╗░███████╗██████╗░██╗░░
+░██╔╝██╔══██╗██║░░░██║██║░░░░░██╔══██╗██╔══██╗██╔════╝██╔══██╗╚██╗░
+██╔╝░██║░░██║╚██╗░██╔╝██║░░░░░██║░░██║██████╔╝█████╗░░██████╔╝░╚██╗
+╚██╗░██║░░██║░╚████╔╝░██║░░░░░██║░░██║██╔═══╝░██╔══╝░░██╔══██╗░██╔╝
+░╚██╗██████╔╝░░╚██╔╝░░███████╗╚█████╔╝██║░░░░░███████╗██║░░██║██╔╝░
+░░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░░░░╚══════╝╚═╝░░╚═╝╚═╝░░
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+            --list\n
+            
+
+
+in: Important Pages
+DVloper
+25
+VIEW SOURCE
+DVloper
+Current Logo
+2017-2019 Logo
+2015-2017 Logo
+Early Logo
+Current Logo
+Dennis Vukanovic, more commonly known as DVloper, is a Swedish game developer who mainly develops horror games for mobile devices. He is known for his hit Granny and Slendrina videogame series.
+He has been making games for 12 years now.
+
+
+Contents
+1	Social Media
+2	Apps Developed by DVloper
+2.1	Slendrina
+2.2	Slendrina: The Cellar
+2.3	Slender Man Rise Again / Slender Man: Stands (formerly)
+2.4	House of Slendrina
+2.5	Robot Hunt
+2.6	Slendrina Asylum
+2.7	Slendrina 2D
+2.8	The Child Of Slendrina
+2.9	Hotel Insanity
+2.10	Slendrina: The School
+2.11	Slendrina: The Cellar 2
+2.12	Slendrina: The Forest
+2.13	Slendrina X
+2.14	Granny
+2.15	Granny: Chapter Two
+2.16	The Twins
+2.17	Granny 3
+2.18	Lost Apps
+3	Trivia
+4	Gallery
+5	References
